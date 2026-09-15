7abf74c1c3434024b660c6b3b31ba73ac2a2d508
Antony-collika <ronbensimpson@gmail.com>
2026-09-15 19:59:11 +0700

    Add MarkdownEscape.kt

---
+++ app/src/main/java/com/example/fts/util/MarkdownEscape.kt
@@ -0,0 +1,22 @@
+package com.example.fts.util
+
+object MarkdownEscape {
+
+    fun escape(text: String): String {
+        return text
+            .replace("*", "\*")
+            .replace("_", "\_")
+            .replace("[", "\[")
+            .replace("]", "\]")
+            .replace("`", "\`")
+            .replace("\", "\\")
+            .replace("(", "\(")
+            .replace(")", "\)")
+            .replace("#", "\#")
+            .replace("+", "\+")
+            .replace("-", "\-")
+            .replace(".", "\.")
+            .replace("!", "\!")
+    }
+
+}
