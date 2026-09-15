7b89c8381600eb084dd8d88d901526c54939e421
Antony-collika <ronbensimpson@gmail.com>
2026-09-15 19:54:47 +0700

    Add app/src/main/java/com/example/fts/data/model/ScanOptions.kt

---
+++ app/src/main/java/com/example/fts/data/model/ScanOptions.kt
@@ -0,0 +1,8 @@
+package com.example.fts.data.model
+
+data class ScanOptions(
+    val skipHiddenFolders: Boolean = true,
+    val skipHiddenFiles: Boolean = true,
+    val skipSystemJunk: Boolean = true,
+    val maxDepth: Int? = null
+)
