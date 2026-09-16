3e13055538886b27507515f9311670ca525cb912
Antony-collika <ronbensimpson@gmail.com>
2026-09-16 18:39:32 +0700

    fix: add scan cancellation action constant

--- app/src/main/java/com/example/fts/util/Constants.kt
+++ app/src/main/java/com/example/fts/util/Constants.kt
@@ -1,27 +1,23 @@
 package com.example.fts.util
 
 object Constants {
-    // Notification
     const val NOTIFICATION_CHANNEL_ID = "fts_scan_channel"
     const val NOTIFICATION_CHANNEL_NAME = "Folder scan"
     const val NOTIFICATION_ID_SCAN = 1001
     const val NOTIFICATION_ID_SCAN_COMPLETE = 1002
-    
-    // Extras - FIX: Use consistent names
+
     const val EXTRA_URI = "extra_uri"
     const val EXTRA_DISPLAY_NAME = "extra_display_name"
     const val EXTRA_INCREMENTAL = "extra_incremental"
-    
-    // Service extras (use same as EXTRA)
+    const val ACTION_CANCEL_SCAN = "com.example.fts.action.CANCEL_SCAN"
+
     const val SERVICE_EXTRA_URI = EXTRA_URI
     const val SERVICE_EXTRA_ROOT_NAME = EXTRA_DISPLAY_NAME
     const val SERVICE_EXTRA_IS_INCREMENTAL = EXTRA_INCREMENTAL
-    
-    // Scan options
+
     const val MAX_DEPTH_UNLIMITED = -1
     const val DEFAULT_MARKDOWN_MODEL = "D"
-    
-    // Cache
+
     const val CACHE_DIR_NAME = "snapshots"
     const val MAX_FOLDERS = 20
 }
