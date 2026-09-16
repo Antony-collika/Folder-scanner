f36d25ced018a07ff39f4afece2c00c9fff8f83e
Antony-collika <ronbensimpson@gmail.com>
2026-09-16 07:51:03 +0700

    Fix missing notification constants

--- app/src/main/java/com/example/fts/util/Constants.kt
+++ app/src/main/java/com/example/fts/util/Constants.kt
@@ -2,11 +2,17 @@ package com.example.fts.util
 
 object Constants {
     const val NOTIFICATION_CHANNEL_ID = "fts_scan_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Folder scan"
     const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_ID_SCAN = 1001
+    const val NOTIFICATION_ID_SCAN_COMPLETE = 1002
     const val FOREGROUND_SERVICE_NOTIFICATION_ID = 1001
     const val EXTRA_URI = "extra_uri"
     const val EXTRA_DISPLAY_NAME = "extra_display_name"
     const val EXTRA_INCREMENTAL = "extra_incremental"
+    const val SERVICE_EXTRA_URI = EXTRA_URI
+    const val SERVICE_EXTRA_ROOT_NAME = EXTRA_DISPLAY_NAME
+    const val SERVICE_EXTRA_IS_INCREMENTAL = EXTRA_INCREMENTAL
     const val MAX_DEPTH_UNLIMITED = -1
     const val DEFAULT_MARKDOWN_MODEL = "D"
     const val CACHE_DIR_NAME = "snapshots"
